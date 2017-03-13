4136fb2af85993db94a56b755b2c5723b48bd950
Kyle Burton <kyle.burton@gmail.com>
2017-03-13 07:56:40 -0700

    upgrade lastpass-cli

--- Formula/lastpass-cli.rb
+++ Formula/lastpass-cli.rb
@@ -1,8 +1,8 @@
 class LastpassCli < Formula
   desc "LastPass command-line interface tool: github.com/kyleburton integration branch"
   homepage "https://github.com/kyleburton/lastpass-cli"
-  url "https://github.com/kyleburton/lastpass-cli/archive/v1.1.3-krb.tar.gz"
-  sha256 "0002a2780951699a29ab08d0d5f4e4384dfa38b9bc09ed587f3372f170719bf1"
+  url "https://github.com/kyleburton/lastpass-cli/archive/v1.1.4-krb.tar.gz"
+  sha256 "f32e435733c99660c5ccb9e78d22b6bae71656b053fae5325a9eafbbf6ac6e07"
   head "https://github.com/lastpass/lastpass-cli.git"
 
   depends_on "asciidoc" => :build
