74c6a9eaa95ea39f2fe04a4dcbf4e73f16f02af3
Kyle Burton <kyle.burton@gmail.com>
2017-08-01 14:17:50 -0700

    make bash-completion a recommended dependency

--- Formula/bake.rb
+++ Formula/bake.rb
@@ -1,11 +1,15 @@
 class Bake < Formula
   version = "1.0.6"
-  desc "Pure bash, very lightweight scripting and build framework."
-  homepage "https://github.com/kyleburton/bake"
-  url "https://github.com/kyleburton/bake/releases/download/#{version}/bake-#{version}.tgz"
-  sha256 "2b7d4006dec823a31556a3ecf80d44002a3b09bb2f1290b80e816ca48cae5118"
+  desc      "Pure bash, very lightweight scripting and build framework."
+  homepage  "https://github.com/kyleburton/bake"
+  url       "https://github.com/kyleburton/bake/releases/download/#{version}/bake-#{version}.tgz"
+  sha256    "2b7d4006dec823a31556a3ecf80d44002a3b09bb2f1290b80e816ca48cae5118"
 
-  depends_on "bash" => :recommended
+  # NB: on OSX bash is really required b/c OS X's bash don't do arrays, while
+  # sane, recent (like since the year 2000) supports arrays.  We should make the depends_on
+  # required for OSX but not for Linuxes
+  depends_on "bash"            => :recommended
+  depends_on "bash-completion" => :recommended
 
   def install
     bin.install "bin/bake"
